0ce1c9b5a931c4abccea7606cff54f8477215c71
Mike <vivancomayta@gmail.com>
2023-10-15 22:53:23 -0500

    image rounded full fixed

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -4,10 +4,10 @@ export default function MyProfilePic() {
   return (
     <section className="w-full mx-auto">
       <Image
-        className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
+        className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto profile-image"
         src="/images/profile.jpg"
-        width={200}
         height={200}
+        width={200}
         alt="Maycol Vivanco"
         priority={true}
       />
